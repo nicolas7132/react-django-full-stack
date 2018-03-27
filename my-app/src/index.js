681d82ba9779b077d087331a8f296a5bb2ec0b6d
kyle <>
2018-03-27 22:55:21 +0800

    react app use real data.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,44 +7,28 @@ import registerServiceWorker from './registerServiceWorker';
 
 var article_list = [];
 axios.get('https://www.uscreditcardguide.com/wp-json/wp/v2/posts/')
-    .then(response => console.log(response))
+    .then(function (rep) {
+       console.log(rep);
+       if(rep.status === 200) {
+           for (var index in rep.data) {
+               var item = {};
+               item["id"] = rep.data[index].id;
+               item["title"] = rep.data[index].title.rendered;
+               item["date"] = rep.data[index].date.substring(0, 10);
+               item["summary"] = rep.data[index].excerpt.rendered;
+               item["url"] = rep.data[index].guid.rendered;
+               article_list.push(item);
+           }
 
-article_list.push({
-    "id":1,
-    "title": "First Title Name",
-    "date": "2018-05-05",
-    "summary": "ssss",
-    "url": "http://kyle.net.cn"
-});
-article_list.push({
-    "id":2,
-    "title": "Second Title Name",
-    "date": "2018-05-06",
-    "summary": "ssss111",
-    "url": "http://kyle.net.cn"
-});
-article_list.push({
-    "id":3,
-    "title": "Third Title Name",
-    "date": "2018-05-07",
-    "summary": "ssss222",
-    "url": "http://kyle.net.cn"
-});
-article_list.push({
-    "id":4,
-    "title": "Four Title Name",
-    "date": "2018-05-08",
-    "summary": "ssss333",
-    "url": "http://kyle.net.cn"
-});
+           if (article_list.length === 0) {
+               document.getElementById("loading-text").innerText = "Get Article Failed!";
+           } else {
+               // hide the loading div
+               document.getElementById("loading").style.display = "none";
 
-if (article_list.length === 0) {
-    document.getElementById("loading-text").innerText = "Get Article Failed!";
-} else {
-    // hide the loading div
-    document.getElementById("loading").style.display = "none";
-
-    ReactDOM.render( <App items={article_list} />, document.getElementById('root'));
-}
+               ReactDOM.render( <App  items = {article_list} />, document.getElementById('root'));
+           }
+       }
+    });
 
 registerServiceWorker();
