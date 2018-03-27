260312efb7509d04b7915e642e82a2dd1bdf2bb3
kyle <>
2018-03-28 01:22:29 +0800

    [django] add model insert and search.

--- my-server/myapp/views.py
+++ my-server/myapp/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from myapp.models import Click
 
 
 def click(request):
-    return HttpResponse("run click() function.")
+    url = request.GET['url']
+    if url is None or url == "":
+        return HttpResponse("Invalid param.")
+
+    records = Click.objects.filter(url=url)
+    if len(records) > 0:
+        records[0].times += 1
+        records[0].save()
+    else:
+        click = Click(url=url, times=1)
+        click.save()
+
+    return HttpResponse("The Click Event saved.")
 
 
 def model(request):
-    return HttpResponse("run model() function.")
+    records = Click.objects.all()
+    if len(records) == 0:
+        return HttpResponse("There is no record now.")
+    else:
+        html_str = ""
+        for rec in records:
+            html_str += str(rec.times) + "\t" + rec.url + "<br>"
+        return HttpResponse(html_str)
