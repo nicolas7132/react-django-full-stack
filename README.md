580ec807a33c5ab2d8f7616c6bf2d11ec4aa3484
kyle <kyle.script@gmail.com>
2018-03-28 10:25:23 +0800

    [django] config template and static file.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # react-django-full-stack
+
+1. complie react demo.
+cd my-app
+npm install
+npm run build
+
+copy "my-app/build" to "my-server/build"
--- my-server/myapp/urls.py
+++ my-server/myapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
+    path('index/', TemplateView.as_view(template_name="index.html")),
     path('click/', views.click, name='click'),
     path('model/', views.model, name='model')
 ]
