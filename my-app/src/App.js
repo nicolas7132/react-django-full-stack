47aa07b3ede72008b5d37c1e487309d12ef62f46
kyle <kyle.script@gmail.com>
2018-03-28 18:13:59 +0800

    [react]send click event to django server

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class Article extends Component {
     handleClick(url, e) {
         e.preventDefault();
+
+        axios.get('/myapp/click?url=' + escape(url))
+            .then(function (rep) {
+                console.log(rep);
+            });
         window.location.href = url;
     }
 
     render() {
         return (<div className="article" onClick={this.handleClick.bind(this, this.props.item.url)}>
-    <h4>{this.props.item.title}</h4>
+        <h4>{this.props.item.title}</h4>
         <p>{this.props.item.summary}</p>
         <span className="time">{this.props.item.date}</span>
         </div>);
@@ -23,8 +29,7 @@ function App(props) {
     return (
         <div>
         {listItems}
-        </div>
-)
+        </div>);
 }
 
 export default App;
